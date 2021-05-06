385c14fcd1f321db140e8ff3a41d3d1e29b26cdd
Hari Kiran Vusirikala <vusirikalaharikiran@gmail.com>
2021-05-07 00:23:27 +0530

    Add deployment files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { RouterModule } from "@angular/router";
 import { HttpClientModule } from "@angular/common/http";
 import { AngularFireModule } from "@angular/fire";
 import { AngularFireAuthModule } from "@angular/fire/auth";
-import { environment } from "src/environments/environment";
+import { environment } from "src/environments/environment.prod";
 
 @NgModule({
   declarations: [AppComponent, FootballCompetitions],
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyC4AUDVzH1xqkQudHbZMg2YW8Q3x1a87B4",
+    authDomain: "my-project-b6423.firebaseapp.com",
+    databaseURL: "https://my-project-b6423.firebaseio.com",
+    projectId: "my-project-b6423",
+    storageBucket: "my-project-b6423.appspot.com",
+    messagingSenderId: "1026860548873",
+    appId: "1:1026860548873:web:c2e64057cb90f726a009d8",
+  },
 };
